6cf936a58117cf03dbe6832d585c7f7e4d026dae
yermaka <ermak.080@mail.ru>
2025-11-03 15:28:57 +0800

    add posts app

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.homepage),
     path("about/", views.about),
+    path("posts/", include("posts.urls")),
 ]
