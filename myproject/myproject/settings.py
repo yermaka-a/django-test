e1637a0a5547c42ad00e83a4c5579b860515e354
yermaka <ermak.080@mail.ru>
2025-11-06 03:27:07 +0800

    add users urls

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "posts",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,11 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     Path(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = Path(BASE_DIR, "media")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
